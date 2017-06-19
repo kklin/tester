934a5f9ac3b3dc634897a01927d046f5777ad28c
Kevin Lin <kevin@quilt.io>
2017-06-19 15:05:25 -0700

    tests: Move integration tests into main repository
    This commit copies the integration tests defined in `quilt/tester` into
    the main Quilt source repo. This way, integration tests will be more
    tightly linked with the code it's testing. For example, new features can
    be merged in the same PR as the accompanying integration test, and
    integration tests will be versioned with releases.

--- tests/elasticsearch/elasticsearch.js
+++
@@ -1,7 +0,0 @@
-const {createDeployment} = require("@quilt/quilt");
-var Elasticsearch = require("@quilt/elasticsearch").Elasticsearch;
-var infrastructure = require("@quilt/tester/config/infrastructure")
-
-var deployment = createDeployment({});
-deployment.deploy(infrastructure);
-deployment.deploy(new Elasticsearch(infrastructure.nWorker).public());
--- tests/etcd/etcd.js
+++
@@ -1,7 +0,0 @@
-const {createDeployment} = require("@quilt/quilt");
-var etcd = require("@quilt/etcd");
-var infrastructure = require("@quilt/tester/config/infrastructure")
-
-var deployment = createDeployment({});
-deployment.deploy(infrastructure);
-deployment.deploy(new etcd.Etcd(infrastructure.nWorker*2));
